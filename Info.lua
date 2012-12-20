e01a50f721bcbf83d10dced5cc58fa9d7571f307
ftischhauser <ftischhauser@gmail.com>
2012-12-20 20:49:37 +0100

    Added the source files and expanded the readme.

---
+++ Info.lua
@@ -0,0 +1,14 @@
+return {
+	LrSdkVersion = 2.0,
+	LrToolkitIdentifier = 'ftischhauser.JPEGOptimizer',
+	LrPluginName = 'JPEG Optimizer',
+	LrPluginInfoUrl = 'http://github.com/ftischhauser/JPEGOptimizer/',
+	VERSION = { major=1, minor=0, revision=0, build=1},
+	LrPluginInfoProvider = 'PluginInfoProvider.lua',
+	LrExportFilterProvider = {
+		title = 'JPEG Optimizer',
+		file = 'JPEGOptimizer.lua',
+		id = 'JPEG Optimizer',
+		supportsVideo = 'false'
+	}
+}
